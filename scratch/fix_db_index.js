aa8ad9b31a3986d13fc2f2019db4f252902c615c
Sittirat <ctf2547@gmail.com>
2026-04-25 23:29:37 +0700

    fix db

--- scratch/fix_db_index.js
+++ scratch/fix_db_index.js
@@ -1,4 +1,4 @@
-const { MongoClient } = require('mongodb');
+import { MongoClient } from 'mongodb';
 const uri = "mongodb+srv://suppachai454_db_user:AKdid33i3GeVfZMB@cluster0.hydec5g.mongodb.net/smartslip_api?retryWrites=true&w=majority&appName=Cluster0";
 
 async function run() {
@@ -8,11 +8,11 @@ async function run() {
     await client.connect();
     const db = client.db('smartslip_api');
     const users = db.collection('users');
-    
+
     console.log('Checking indexes for collection: users');
     const indexes = await users.listIndexes().toArray();
     console.log('Current indexes:', JSON.stringify(indexes, null, 2));
-    
+
     const lineUserIdIndex = indexes.find(idx => idx.name === 'lineUserId_1');
     if (lineUserIdIndex) {
       console.log('Dropping lineUserId_1 index...');
